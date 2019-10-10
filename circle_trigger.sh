d0e8e6fd987b431440f28ccee41397763370b8ce
Dumitru Deveatii <dumitru@labs42.io>
2019-10-10 15:56:10 +0300

    Improved change detection Get the commit SHA of the last successful build (previously was just last build).

--- circle_trigger.sh
+++ circle_trigger.sh
@@ -11,7 +11,7 @@ CIRCLE_API="https://circleci.com/api"
 ## 1. Commit SHA of last CI build
 ############################################
 LAST_COMPLETED_BUILD_URL="${CIRCLE_API}/v1.1/project/${REPOSITORY_TYPE}/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/tree/${CIRCLE_BRANCH}?filter=completed&limit=1&shallow=true"
-LAST_COMPLETED_BUILD_SHA=`curl -Ss -u ${CIRCLE_TOKEN}: ${LAST_COMPLETED_BUILD_URL} | jq -r '.[0]["vcs_revision"]'`
+LAST_COMPLETED_BUILD_SHA=`curl -Ss -u ${CIRCLE_TOKEN}: ${LAST_COMPLETED_BUILD_URL} | jq -r 'map(select(.status == "success")) | .[0]["vcs_revision"]'`
 
 if  [[ ${LAST_COMPLETED_BUILD_SHA} == "null" ]]; then
   echo -e "\e[93mThere are no completed CI builds in branch ${CIRCLE_BRANCH}.\e[0m"
