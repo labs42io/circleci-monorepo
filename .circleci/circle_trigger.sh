951e5b3bb0b07a812a6a5c1e0a7a7b7773823247
Dumitru Deveatii <dumitru@labs42.io>
2019-11-08 14:54:29 +0200

    fix: adjust http status code

--- .circleci/circle_trigger.sh
+++ .circleci/circle_trigger.sh
@@ -90,13 +90,13 @@ echo -e "  $DATA"
 URL="${CIRCLE_API}/v2/project/${REPOSITORY_TYPE}/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/pipeline"
 HTTP_RESPONSE=$(curl -s -u ${CIRCLE_TOKEN}: -o response.txt -w "%{http_code}" -X POST --header "Content-Type: application/json" -d "$DATA" $URL)
 
-if [ $HTTP_RESPONSE != "202" ]; then
+if [ "$HTTP_RESPONSE" -ge "200" ] && [ "$HTTP_RESPONSE" -lt "300" ]; then
+    echo "API call succeeded."
+    echo "Response:"
+    cat response.txt
+else
     echo -e "\e[93mReceived status code: ${HTTP_RESPONSE}\e[0m"
     echo "Response:"
     cat response.txt
     exit 1
-else
-    echo "API call succeeded."
-    echo "Response:"
-    cat response.txt
 fi
