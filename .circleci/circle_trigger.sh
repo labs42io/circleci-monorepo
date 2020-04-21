ca7cf2094e754017a63573d06e6aae6d11038b70
Tovieye Moses Ozi <tovieye.ozi@gmail.com>
2020-04-21 19:03:45 +0100

    convert branch to full sha1
    Addresses a CircleCI issue.
    only git sha1 are allowed in git revision ranges
    
    Fixes #9

--- .circleci/circle_trigger.sh
+++ .circleci/circle_trigger.sh
@@ -48,7 +48,7 @@ fi
 
 if [[ ${LAST_COMPLETED_BUILD_SHA} == "null" ]]; then
   echo -e "\e[93mNo CI builds for branch ${PARENT_BRANCH}. Using master.\e[0m"
-  LAST_COMPLETED_BUILD_SHA=master
+  LAST_COMPLETED_BUILD_SHA=$(git rev-parse origin/master)
 fi
 
 ############################################
